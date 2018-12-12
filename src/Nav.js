bd73b5b32a16bff77641ca787f83a0d89911117d
sumitsharmaads <sumit.sharma@affluencesolutions.com>
2018-12-12 01:25:50 -0500

    adding new files

---
+++ src/Nav.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class Nav extends Component {
+  render() {
+    return (
+      <div className="Nav">
+      <h1> This is Navigation </h1>
+      </div>
+    );
+  }
+}
+
+export default Nav;
---
+++ src/Section.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class Section extends Component {
+  render() {
+    return (
+      <div className="Section">
+      <h1> This is Navigation </h1>
+      </div>
+    );
+  }
+}
+
+export default Section;
